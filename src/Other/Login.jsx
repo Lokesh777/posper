eb9723fdfee5b207589e976d9d29d74287a9c194
Lokesh777 <lokeshdevgan777@gmail.com>
2023-04-14 01:33:35 +0530

    update

--- src/Other/Login.jsx
+++ src/Other/Login.jsx
@@ -1,11 +1,9 @@
-import {  useContext, useState } from 'react';
+import { useState } from 'react';
 import { TextField, Button, Box, Typography } from '@mui/material';
 import axios from "axios";
-import { Context } from '../context/Context';
 import "./All.css";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
 import { api_url } from '../API/Api';
 
 
@@ -20,50 +18,9 @@ const initialFormData = {
   email: "",
   password: "",
 };
-
-export default function Authentication({handleAuth}) {
-  const [loginSignup, setLoginSignup] = useState(true);
+export default function Authentication() {
   const [formData, setFormData] = useState(initialFormData);
-  const [error,setError]=useState(false);
-  const {dispatch, isFetching } = useContext(Context);
-  const navigate = useNavigate()
-
-  const loginSubmit = async (e) => {
-    e.preventDefault();
-    setError(false)
-  dispatch({type:"LOGIN_START"})
-  try{
-    const response = await axios.post(`${api_url}auth/login`,{
-      email: formData.email,
-      password: formData.password
-    });
-
-    if(!response){
-      toast.error(response.data.message);
-    }
-    dispatch({type: "LOGIN_SUCCESS", payload: response.data})
-    // alert(response.data.message);    
-    toast.success(response.data.message);
-    navigate("/");
-    console.log(response.data);
-    handleAuth()
-
-  }catch(err){
-    setError(true);
-    dispatch({type:"LOGIN_FAILURE"});
-    if (err.response) {
-      // alert(err.response.data.message);
-      toast.error(err.response.data.message);
-      console.log(err.response.data);
-    } else if (err.request) {
-      console.log(err.request);
-    } else {
-      toast.error(err.message)
-      console.log('Error', err.message);
-    }
-  }
-
-  }
+  const [error, setError] = useState(false); 
 
   const signupSubmit = async (e) => {
     e.preventDefault();
@@ -71,164 +28,161 @@ export default function Authentication({handleAuth}) {
     try {
       let res = await axios.post(`${api_url}auth/signup`, formData);
       toast.success(res.data.message); 
-      console.log(res.data.newUser);
-      navigate("/login")
+    
     } catch (err) {
-      setError(true);
-      console.log(err);
+        setError(true);
+      console.log(err.message,err);
       toast.error(err.response.data.message)
     }
   }
-
- // events and functions
+  // events and functions
   const handleChange = (e) => {
     const { name, value } = e.target;
+    
     setFormData({ ...formData, [name]: value });
-  }
-  
-  return (    
-    <form onSubmit={loginSignup ? signupSubmit:loginSubmit}>
-      <ToastContainer position="top-center" />
-          <Box 
-              display="flex" 
-              flexDirection={"column"}
-              maxWidth={400} 
-              textAlign="center"
-              justifyContent={"center"}
-              // margin="auto"
-              padding={1}
-              marginTop={1}
-            
-            >
-          <Typography fontWeight={600} fontSize={30}>
-          {loginSignup?"Signup":"Login" }
-            </Typography>
-            
-            <div style={{display:"flex"}}>
-
-            {loginSignup&&
-                <TextField
-                    margin="normal"
-                    variant="outlined"
-                    label="First Name"
-                    name='firstName'
-                    value={formData.firstName}
-                    onChange={handleChange}
-                />
-            }    
-
-            {loginSignup&&
-              <TextField
-                margin="normal"
-                variant="outlined"
-                label="Middle Name"
-                name='middleName'
-                value={formData.middleName}
-                onChange={handleChange}
-              />
-            } 
-            </div>
-
-            <div style={{display:"flex"}}>
-                {loginSignup&&
-                  <TextField
-                    margin="normal"
-                    variant="outlined"
-                    label="Last Name"
-                    name='lastName'
-                    value={formData.lastName}
-                    onChange={handleChange}
-                  />
-                } 
-
-                  {loginSignup&&
-                  <TextField
-                    margin="normal"
-                    variant="outlined"
-                    label="Organisation Name"
-                    name='organisationName'
-                    value={formData.organisationName}
-                    onChange={handleChange}
-                  />
-                } 
-            </div>
-
-            <div style={{display:"flex"}}>
-                      {loginSignup&&
-                        <TextField
-                            margin="normal"
-                            variant="outlined"
-                            label="GST Number"
-                            name='gstNumber'
-                            value={formData.gstNumber}
-                            onChange={handleChange}
-                        />
-                    } 
-
-                    {loginSignup&&
-                    <TextField
-                      margin="normal"
-                      variant="outlined"
-                      label="Mobile Number"
-                      name='mobileNumber'
-                      value={formData.mobileNumber}
-                      onChange={handleChange}
-                    />
-                    } 
-            </div>
-
-            <div style={{display:"flex"}}>
-                {loginSignup&&
-                    <TextField
-                      margin="normal"
-                      variant="outlined"
-                      label="Address"
-                      name='address'
-                      value={formData.address}
-                      onChange={handleChange}
-                    />
-                  } 
-
-                <TextField
-                margin="normal"
-                variant="outlined"
-                  label="Email"
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  
-                />
-            </div>
-
-            <TextField
-            margin="normal"
-              variant="outlined"
-              label="Password"
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              
-            />
-            
+  };
 
 
-            <Button disabled={isFetching}
-            className="loginButton"
-            sx={{marginTop:"5px"}}
-            variant="contained" color="warning" type="submit">
-              Submit
-            </Button>
 
-            <Typography onClick={()=>setLoginSignup(!loginSignup)} 
-            paddingTop={1} sx={{fontWeight:"bolder"}} > 
-            Change to {loginSignup ? "Login":"Signup"}
-            </Typography>
-        
-          {error && <p style={{color:'red'}} >Something went wrong!!!</p>}
-          </Box>
-    </form>
-  );
+return (
+  <form onSubmit={signupSubmit}>
+    <ToastContainer position="top-center" />
+    <Box
+      display="flex"
+      flexDirection={"column"}
+      maxWidth={400}
+      textAlign="center"
+      justifyContent={"center"}
+      margin="auto"
+      padding={1}
+      marginTop={1}
+
+    >
+      <Typography fontWeight={600} fontSize={30}>
+        Signup
+      </Typography>
+
+      {error && <p style={{ color: 'red' }} >Something went wrong!!!</p>}
+
+
+      <div style={{ display: "flex", gap: "1rem" }}>
+
+
+        <TextField
+          margin="normal"
+
+          label="First Name"
+          name='firstName'
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+
+        <TextField
+          margin="normal"
+
+          label="Middle Name"
+          name='middleName'
+          value={formData.middleName}
+          onChange={handleChange}
+        />
+
+      </div>
+
+      <div style={{ display: "flex", gap: "1rem" }}>
+
+        <TextField
+          margin="normal"
+
+          label="Last Name"
+          name='lastName'
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+
+
+
+        <TextField
+          margin="normal"
+
+          label="Organisation Name"
+          name='organisationName'
+          value={formData.organisationName}
+          onChange={handleChange}
+        />
+
+      </div>
+
+      <div style={{ display: "flex", gap: "1rem" }}>
+
+        <TextField
+          margin="normal"
+          label="GST Number"
+          name='gstNumber'
+          value={formData.gstNumber}
+          onChange={handleChange}
+        />
+
+        <TextField
+          margin="normal"
+
+          label="Mobile Number"
+          name='mobileNumber'
+          value={formData.mobileNumber}
+          onChange={handleChange}
+          inputProps={{ min: "10", max: "12", step: "1" }}
+          required
+        />
+
+      </div>
+
+
+      <TextField
+        margin="normal"
+
+        label="Address"
+        name='address'
+        value={formData.address}
+        onChange={handleChange}
+      />
+
+      <div style={{ display: "flex", gap: "1rem" }}>
+
+        <TextField
+          margin="normal"
+
+          label="Email"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+
+        />
+        <TextField
+          margin="normal"
+
+          label="Password"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+
+        />
+      </div>
+
+
+
+
+      <Button
+        className="loginButton"
+        sx={{ marginTop: "5px" }}
+        variant="contained" color="warning" type="submit">
+        Submit
+      </Button>
+
+
+
+    </Box>
+  </form>
+);
 };
 
